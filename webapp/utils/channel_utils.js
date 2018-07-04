7415c24ea2e9b818cfd5e62a660e735233943dd4
YanPeng <yanpeng.gao@gmail.com>
2018-07-04 16:50:54 -0400

    fixed user_utils.js where scrutinizer found bug

--- webapp/utils/channel_utils.js
+++ webapp/utils/channel_utils.js
@@ -1,10 +1,8 @@
 import {getChannelByNameAndTeamName} from 'mattermost-redux/actions/channels';
 
 export function getDirectChannel(teamname, username) {
-  console.log("teamname: " + teamname)
-  console.log("username: " + username)
   if (username) {
-    channel = getChannelByNameAndTeamName(teamname, username);
+    var channel = getChannelByNameAndTeamName(teamname, username);
     return channel;
   }
   return {};
